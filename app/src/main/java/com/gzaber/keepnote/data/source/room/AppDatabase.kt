a4ac7cd5400f43cda221b09a31779ea0d01d8b92
Grzesiek <gzaber@tlen.pl>
2023-09-12 21:45:27 +0200

    move Converters to a separate file

--- app/src/main/java/com/gzaber/keepnote/data/source/room/AppDatabase.kt
+++ app/src/main/java/com/gzaber/keepnote/data/source/room/AppDatabase.kt
@@ -6,21 +6,10 @@ import androidx.room.TypeConverter
 import androidx.room.TypeConverters
 import java.util.Date
 
-@Database(entities = [FolderEntity::class, NoteEntity::class], version = 1)
+@Database(entities = [FolderEntity::class, NoteEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun folderDao(): FolderDao
     abstract fun noteDao(): NoteDao
 }
 
-class Converters {
-    @TypeConverter
-    fun fromTimestamp(value: Long?): Date? {
-        return value?.let { Date(it) }
-    }
-
-    @TypeConverter
-    fun dateToTimestamp(date: Date?): Long? {
-        return date?.time
-    }
-}
