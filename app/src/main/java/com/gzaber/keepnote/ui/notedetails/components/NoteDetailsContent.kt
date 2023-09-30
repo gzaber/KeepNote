b6b52122b8b6108d9b290b4417ffc8b924cbe9a8
Grzesiek <gzaber@tlen.pl>
2023-09-30 09:07:19 +0200

    add vertical scroll to column

--- app/src/main/java/com/gzaber/keepnote/ui/notedetails/components/NoteDetailsContent.kt
+++ app/src/main/java/com/gzaber/keepnote/ui/notedetails/components/NoteDetailsContent.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,6 +30,7 @@ fun NoteDetailsContent(
     Column(
         modifier = modifier
             .padding(contentPadding)
+            .verticalScroll(rememberScrollState())
     ) {
         DetailsHeader(
             title = note.title,
